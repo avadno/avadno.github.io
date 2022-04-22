24afeabc6535ba40d88c785618dac182cccfac9c
avadno <104193082+avadno@users.noreply.github.com>
2022-04-22 04:24:54 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+function str2arr(str) {
+  var arr = [];
+  var s = "";
+  for(var i = 0; i < str.length; i++) {
+    if(str[i] == "\n") {
+      if(s != "") arr.push(s);
+      s = "";
+    }
+    else s += str[i];
+  }
+  return arr;
+}
+
+function arrChoice(arr) {
+  var i = Math.floor(Math.random() * arr.length);
+  return arr[i];
+}
+
+function generate() {
+  var adv = arrChoice(str2arr(adverbs));
+  var adj = arrChoice(str2arr(adjectives));
+  var nou = arrChoice (str2arr(nouns));
+  document.getElementById("output").innerHTML = adv + " " + adj + " " + nou;
+}
+
+generate();
